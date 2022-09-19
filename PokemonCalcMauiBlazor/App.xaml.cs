9bff148f042f210b7efd992f30c747736a5f8ca3
Iyarashii <strider1337v@gmail.com>
2022-09-19 19:59:42 +0200

    Maximize app on startup on Windows

--- PokemonCalcMauiBlazor/App.xaml.cs
+++ PokemonCalcMauiBlazor/App.xaml.cs
@@ -1,11 +1,24 @@
-﻿namespace PkmnCalcMauiBlazor;
+﻿using System.Runtime.InteropServices;
+
+namespace PkmnCalcMauiBlazor;
 
 public partial class App : Application
 {
-	public App()
+    public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+#if WINDOWS
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            ShowWindow(windowHandle, 3);
+#endif
+        });
+        MainPage = new MainPage();
 	}
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
 }
