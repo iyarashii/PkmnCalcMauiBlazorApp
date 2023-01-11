7c8bd464457f89b1ec01d4f76d8edcadcf243dc6
Iyarashii <strider1337v@gmail.com>
2023-01-11 18:58:33 +0100

    Add dex logic folder

---
+++ PokemonCalcMauiBlazor/Pages/Logic/FullPokedex.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages.Logic
+{
+    public class FullPokedex : IPokedexType
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string Url => throw new NotImplementedException();
+
+        public string Regex => throw new NotImplementedException();
+    }
+}
