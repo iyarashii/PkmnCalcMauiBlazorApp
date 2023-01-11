b6853ee2b0874b2a1664d5cc947a8c55f14a0dc3
Iyarashii <strider1337v@gmail.com>
2023-01-11 15:56:08 +0100

    Add constants folder

---
+++ PokemonCalcMauiBlazor/Constants/FullPokedex.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Constants
+{
+    public static class FullPokedex
+    {
+        public const string Name = "Full Pokédex";
+
+        public const string NameSearchRegex = "";
+
+        public const string Url = "https://www.serebii.net/pokemon/";
+    }
+}
