80258dca14ccce4ae547957f953f1e34193a2635
Iyarashii <strider1337v@gmail.com>
2023-01-14 16:21:21 +0100

    Add base class for dex type

---
+++ PokemonCalcMauiBlazor/Pages/Logic/PokedexType.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages.Logic
+{
+    public abstract class PokedexType : IPokedexType
+    {
+        public abstract string Name { get; }
+        public abstract string Url { get; }
+        public abstract string Regex { get; }
+        public abstract string FileName { get; }
+
+        // Note: this is important so the MudSelect can compare pokedex types
+        public override bool Equals(object o)
+        {
+            var other = o as PokedexType;
+            return other?.Name == Name;
+        }
+        // Note: this is important too!
+        public override int GetHashCode() => Name?.GetHashCode() ?? 0;
+
+        // display PokedexType correctly in MudSelect
+        public override string ToString() => Name;
+    }
+}
