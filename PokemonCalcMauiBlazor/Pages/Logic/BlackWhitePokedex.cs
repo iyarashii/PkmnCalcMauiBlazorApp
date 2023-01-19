e24d34da3520789bd49b65f5b6e0347b98dae707
Iyarashii <strider1337v@gmail.com>
2023-01-19 20:20:20 +0100

    Add black/white dex

---
+++ PokemonCalcMauiBlazor/Pages/Logic/BlackWhitePokedex.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages.Logic
+{
+    public class BlackWhitePokedex : PokedexType
+    {
+        public override string Name => "Black/White Pokédex";
+
+        public override string Url => "https://www.serebii.net/pokedex-bw/";
+
+        public override string Regex => throw new NotImplementedException();
+
+        public override string FileName => "pokedex-bw.txt";
+    }
+}
