9532de26af8731897384be6aedfc356f3cf4fe68
Iyarashii <strider1337v@gmail.com>
2023-01-25 17:12:23 +0100

    Add legends dex

---
+++ PokemonCalcMauiBlazor/Pages/Logic/LegendsPokedex.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages.Logic
+{
+    public class LegendsPokedex : SwordShieldPokedex
+    {
+        public override string Name => "Legends Pokédex";
+    }
+}
