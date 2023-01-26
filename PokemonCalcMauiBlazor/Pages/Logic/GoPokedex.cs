f8ec9d24a213a8a62eaa3f463ca0db4afa15c4d5
Iyarashii <strider1337v@gmail.com>
2023-01-26 20:38:01 +0100

    Add GO dex

---
+++ PokemonCalcMauiBlazor/Pages/Logic/GoPokedex.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages.Logic
+{
+    public class GoPokedex : PokedexType
+    {
+        public override string Name => "GO Pokédex";
+
+        public override string Url => "https://www.serebii.net/pokemongo/pokemon/";
+
+        public override string Regex => throw new NotImplementedException();
+
+        public override string FileName => "pokedex-go.txt";
+    }
+}
