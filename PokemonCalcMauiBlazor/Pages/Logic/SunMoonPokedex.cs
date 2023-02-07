ca734cd721d82914fc39b6f63c21a48ba8f5b849
Iyarashii <strider1337v@gmail.com>
2023-02-07 20:05:15 +0100

    Update regex

--- PokemonCalcMauiBlazor/Pages/Logic/SunMoonPokedex.cs
+++ PokemonCalcMauiBlazor/Pages/Logic/SunMoonPokedex.cs
@@ -15,7 +15,7 @@ namespace PkmnCalcMauiBlazor.Pages.Logic
 
         public override string Url => "https://www.serebii.net/pokedex-sm/";
 
-        public override string Regex => throw new NotImplementedException();
+        public override string Regex => "(?<=<option value=\"/pokedex-sm/)(\\d.*)(?=\\s)";
 
         public override string FileName => "pokedex-sm.txt";
     }
