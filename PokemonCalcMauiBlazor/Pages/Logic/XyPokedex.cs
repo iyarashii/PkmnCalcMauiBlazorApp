c0dcb848c2fd5796914197cbc148b65de4a0d9e9
Iyarashii <strider1337v@gmail.com>
2023-02-12 19:13:52 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/Pages/Logic/XyPokedex.cs
+++ PokemonCalcMauiBlazor/Pages/Logic/XyPokedex.cs
@@ -1,12 +1,6 @@
 ﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PkmnCalcMauiBlazor.Pages.Logic
 {
     public class XyPokedex : PokedexType
