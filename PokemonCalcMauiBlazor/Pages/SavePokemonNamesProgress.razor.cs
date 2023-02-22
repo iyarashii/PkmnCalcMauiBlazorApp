e23312b801dbdc7916ad550aa1ed9a16474d75c8
Iyarashii <strider1337v@gmail.com>
2023-02-22 17:40:05 +0100

    Use code-behind for progress component

---
+++ PokemonCalcMauiBlazor/Pages/SavePokemonNamesProgress.razor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+
+namespace PkmnCalcMauiBlazor.Pages
+{
+    public partial class SavePokemonNamesProgress : ComponentBase
+    {
+        private double _value;
+        [Parameter]
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value) return;
+                _value = value;
+                if (_disposed)
+                {
+                    return;
+                }
+                StateHasChanged();
+            }
+        }
+        bool _disposed;
+        public void Dispose() => _disposed = true;
+    }
+}
