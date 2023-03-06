17ce5b8d7519abef9ba74da00766973562d63cf8
Iyarashii <strider1337v@gmail.com>
2023-03-06 19:02:40 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/Pages/Pokedex.razor.cs
+++ PokemonCalcMauiBlazor/Pages/Pokedex.razor.cs
@@ -3,12 +3,7 @@
 
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using PkmnCalcMauiBlazor.Pages.Logic;
 
 namespace PkmnCalcMauiBlazor.Pages
