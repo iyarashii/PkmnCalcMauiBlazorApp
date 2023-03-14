f101871048e69ebff31dcdb176ce59285f1b7dc7
Iyarashii <strider1337v@gmail.com>
2023-03-14 19:24:08 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/Pages/PokedexSelect.razor.cs
+++ PokemonCalcMauiBlazor/Pages/PokedexSelect.razor.cs
@@ -2,11 +2,6 @@
 // Licensed under the GNU General Public License v3.0.
 
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PkmnCalcMauiBlazor.Pages.Logic;
 
 namespace PkmnCalcMauiBlazor.Pages
