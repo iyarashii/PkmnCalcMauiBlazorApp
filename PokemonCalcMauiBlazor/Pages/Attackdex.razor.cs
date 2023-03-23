07b496a8c48a9d6e87b46663bd2607f15d04c2a5
Iyarashii <strider1337v@gmail.com>
2023-03-23 19:09:26 +0100

    Add dex select component for attackdex

--- PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
+++ PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
@@ -2,6 +2,7 @@
 // Licensed under the GNU General Public License v3.0.
 
 using Microsoft.AspNetCore.Components;
+using PkmnCalcMauiBlazor.Pages.Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,42 @@ namespace PkmnCalcMauiBlazor.Pages
 {
     public partial class Attackdex : BasePage
     {
-
+        private static IDexType selectedAttackdexType = new ScarletVioletPokedex();
+#if DEBUG && WINDOWS
+        private static string pathToData = $@"G:\repos\PkmnCalcMauiBlazor\PokemonCalcMauiBlazor\Data\{SelectedAttackdexType.FileName}";
+#else
+        private static string pathToPokemonNames = Path.Combine(FileSystem.Current.AppDataDirectory, SelectedAttackdexType.FileName);
+#endif
+        private readonly List<IDexType> attackdexTypes = new()
+        {
+            new FullPokedex(),
+            new RedBluePokedex(),
+            new GoldSilverPokedex(),
+            new RubySapphirePokedex(),
+            new DiamondPearlPokedex(),
+            new BlackWhitePokedex(),
+            new XyPokedex(),
+            new SunMoonPokedex(),
+            new LetsGoPokedex(),
+            new SwordShieldPokedex(),
+            new BdspPokedex(),
+            new LegendsPokedex(),
+            new GoPokedex(),
+            new ScarletVioletPokedex()
+        };
+        public static string AttackdexSource { get; set; } = "https://www.serebii.net/pokedex-sv/";
+        public static IDexType SelectedAttackdexType
+        {
+            get => selectedAttackdexType;
+            set
+            {
+                selectedAttackdexType = value;
+#if DEBUG && WINDOWS
+                pathToData = $@"G:\repos\PkmnCalcMauiBlazor\PokemonCalcMauiBlazor\Data\{SelectedAttackdexType.FileName}";
+#else
+                pathToPokemonNames = Path.Combine(FileSystem.Current.AppDataDirectory, SelectedAttackdexType.FileName);
+#endif
+            }
+        }
     }
 }
