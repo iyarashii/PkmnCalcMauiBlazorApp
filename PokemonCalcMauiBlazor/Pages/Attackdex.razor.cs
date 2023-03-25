b13d7d55a17b1eda513c4990403d905248ac08e4
Iyarashii <strider1337v@gmail.com>
2023-03-25 16:25:22 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
+++ PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0.
 
-using Microsoft.AspNetCore.Components;
 using PkmnCalcMauiBlazor.Pages.Logic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace PkmnCalcMauiBlazor.Pages
 {
