dc08ec46a560f9f288069586a3b39a3c9bf3f79f
Iyarashii <strider1337v@gmail.com>
2023-03-28 20:37:30 +0200

    Move logic to code behind file

---
+++ PokemonCalcMauiBlazor/Pages/SavePokemonDataDialog.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Pages
+{
+    public partial class SavePokemonDataDialog
+    {
+        [CascadingParameter]
+        MudDialogInstance MudDialog { get; set; }
+
+        void Submit() => MudDialog.Close(DialogResult.Ok(true));
+        void Cancel() => MudDialog.Cancel();
+    }
+}
