9c6bd46d1993f6b1f5d220b7d275968df5c32025
Iyarashii <strider1337v@gmail.com>
2023-06-07 23:19:32 +0200

    Remove attackdex from name

--- PokemonCalcMauiBlazor/Pages/Logic/Attackdex/RedBlueYellowAttackdex.cs
+++ PokemonCalcMauiBlazor/Pages/Logic/Attackdex/RedBlueYellowAttackdex.cs
@@ -5,7 +5,7 @@ namespace PkmnCalcMauiBlazor.Pages.Logic.Attackdex
 {
     public class RedBlueYellowAttackdex : DexType
     {
-        public override string Name => "Red/Blue/Yellow Attackdex";
+        public override string Name => "Red/Blue/Yellow";
 
         public override string Url => "https://www.serebii.net/attackdex-rby/";
 
