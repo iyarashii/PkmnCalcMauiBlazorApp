957b0fe157b3a1c17a3c59d6e3107ea054d60d50
Iyarashii <strider1337v@gmail.com>
2023-06-15 16:53:39 +0200

    Fix name and cleanup

--- PokemonCalcMauiBlazor/Pages/Logic/GoldSilverPokedex.cs
+++ PokemonCalcMauiBlazor/Pages/Logic/GoldSilverPokedex.cs
@@ -1,17 +1,11 @@
 ﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PkmnCalcMauiBlazor.Pages.Logic
 {
     public class GoldSilverPokedex : DexType
     {
-        public override string Name => "Gold/Silver Pokédex";
+        public override string Name => "Gold / Silver";
 
         public override string Url => "https://www.serebii.net/pokedex-gs/";
 
