1b63d1915b343b471a80fadc5573b4eea0539796
Iyarashii <strider1337v@gmail.com>
2023-07-01 12:54:14 +0200

    Cleanup

--- PokemonCalcMauiBlazor/Pages/Logic/FullPokedex.cs
+++ PokemonCalcMauiBlazor/Pages/Logic/FullPokedex.cs
@@ -1,12 +1,6 @@
 ﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PkmnCalcMauiBlazor.Pages.Logic
 {
     public class FullPokedex : DexType
