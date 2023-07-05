dcefcb247dc1d382cea870f8fe42e7390500dfeb
Iyarashii <strider1337v@gmail.com>
2023-07-05 22:33:18 +0200

    Add unit test for component

---
+++ PkmnCalcMauiBlazor.Tests/PokemonTypeCalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+using PkmnCalcMauiBlazor.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkmnCalcMauiBlazor.Tests
+{
+	public class PokemonTypeCalculatorTests : TestContext
+	{
+		[Fact]
+		public void VerifyPrimaryTypeDefaultSelection()
+		{
+            // Arrange
+            Services.AddMudServices();
+			JSInterop.SetupVoid("mudPopover.initialize", "mudblazor-main-content", 0);
+			JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
+			var cut = RenderComponent<PokemonTypeCalculator>();
+			var primaryTypeSelect = cut.FindComponent<PokemonTypeSelect>();
+
+			// Assert
+			Assert.Equal("(none)", primaryTypeSelect.Instance.SelectedTypeName);
+		}
+	}
+}
