da568f3feafa04f6c1894eeb1dc7eda523050e16
Iyarashii <strider1337v@gmail.com>
2023-09-15 23:02:59 +0200

    Cleanup

--- PkmnCalcMauiBlazor.Tests/PokemonTypeCalculatorTests.cs
+++ PkmnCalcMauiBlazor.Tests/PokemonTypeCalculatorTests.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using MudBlazor.Services;
+﻿using MudBlazor.Services;
 using PkmnCalcMauiBlazor.Pages;
 using PokemonTypeLibrary.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PkmnCalcMauiBlazor.Tests
 {
