26ed5aed06126e25568c5b3026a6b02d4df9b077
Iyarashii <strider1337v@gmail.com>
2023-12-26 20:31:21 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/MauiProgram.cs
+++ PokemonCalcMauiBlazor/MauiProgram.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) 2022 iyarashii @ https://github.com/iyarashii 
 // Licensed under the GNU General Public License v3.0.
 
-using Microsoft.AspNetCore.Components.WebView.Maui;
 using MudBlazor.Services;
 
 namespace PkmnCalcMauiBlazor;
