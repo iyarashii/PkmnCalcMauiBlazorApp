a60fb992ae940311186d7b03e4c7ee2ddfcd154c
Iyarashii <strider1337v@gmail.com>
2024-01-16 18:29:10 +0100

    Cleanup

--- PkmnCalcMauiBlazor.Tests/PokedexTests.cs
+++ PkmnCalcMauiBlazor.Tests/PokedexTests.cs
@@ -1,42 +1,40 @@
 ﻿using MudBlazor.Services;
 using NSubstitute;
 using PkmnCalcMauiBlazor.Pages;
-using System;
 using System.IO;
 using System.IO.Abstractions;
 using System.Threading.Tasks;
-using Xunit;
 
 namespace PkmnCalcMauiBlazor.Tests
 {
     public class PokedexTests : TestContext
     {
-		public PokedexTests()
-		{
-			// Mud services test setup
-			Services.AddMudServices();
+        public PokedexTests()
+        {
+            // Mud services test setup
+            Services.AddMudServices();
             Services.AddSingleton<IFileSystem, FileSystem>();
-			JSInterop.SetupVoid("mudPopover.initialize", "mudblazor-main-content", 0);
-			JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
-		}
+            JSInterop.SetupVoid("mudPopover.initialize", "mudblazor-main-content", 0);
+            JSInterop.SetupVoid("mudKeyInterceptor.connect", _ => true);
+        }
 
-		[Fact]
-		public void GetPokedexPageName_PokemonNameWithWhitespace_ReturnsLowercaseWithoutSpace()
-		{
+        [Fact]
+        public void GetPokedexPageName_PokemonNameWithWhitespace_ReturnsLowercaseWithoutSpace()
+        {
             // Arrange
             string testName = "test Pokemon NAME";
-			Pokedex.PokemonName = testName;
-			var cut = RenderComponent<Pokedex>();
-			// Act
-			var result = Pokedex.GetPokedexPageName();
+            Pokedex.PokemonName = testName;
+            var cut = RenderComponent<Pokedex>();
+            // Act
+            var result = Pokedex.GetPokedexPageName();
             // Assert
             Assert.NotNull(cut);
             Assert.Equal("testpokemonname", result);
-		}
+        }
 
-		[Fact]
-		public void GetPokedexPageName_PokemonNameStartsWithDigit_Returns3DigitsWithShtml()
-		{
+        [Fact]
+        public void GetPokedexPageName_PokemonNameStartsWithDigit_Returns3DigitsWithShtml()
+        {
             // Arrange
             string testName = "393";
             Pokedex.PokemonName = testName;
@@ -71,7 +69,7 @@ namespace PkmnCalcMauiBlazor.Tests
             var savePokemonNamesTask = cut.InvokeAsync(() => cut.Instance.SavePokemonNames());
             while (savePokemonNamesTask.Status != TaskStatus.RanToCompletion)
             {
-                if(cut.Instance.progressVisible && cut.Instance.SaveProgress != 0.0)
+                if (cut.Instance.progressVisible && cut.Instance.SaveProgress != 0.0)
                 {
                     Assert.True(cut.Instance.SaveProgress > 0.0);
                     break;
