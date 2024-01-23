ccb541d262ed074678fce20e99c18d806c76555e
Iyarashii <strider1337v@gmail.com>
2024-01-23 20:50:51 +0100

    Simplify init

--- PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
+++ PokemonCalcMauiBlazor/Pages/Attackdex.razor.cs
@@ -70,7 +70,7 @@ namespace PkmnCalcMauiBlazor.Pages
             {
                 string downloadString = await client.GetStringAsync(AttackdexSource);
                 var nameMatches = Regex.Matches(downloadString, SelectedAttackdexType.Regex);
-                HashSet<string> namesToSave = new();
+                HashSet<string> namesToSave = [];
                 void formatAndSaveName(Match nameMatch) => namesToSave.Add(Regex.Replace(nameMatch.Value.Trim(), "</option>", ""));
                 var progress = 0.0;
                 foreach (Match nameMatch in nameMatches.Cast<Match>())
