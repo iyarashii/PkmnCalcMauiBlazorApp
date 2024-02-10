8fab6b71275d11fada4825f11c0148ff19b02f70
Iyarashii <strider1337v@gmail.com>
2024-02-10 17:15:11 +0100

    Add project voltage

---
+++ PokemonCalcMauiBlazor/Pages/Voltage.razor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023 iyarashii @ https://github.com/iyarashii 
+// Licensed under the GNU General Public License v3.0.
+
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PkmnCalcMauiBlazor.Pages
+{
+    public partial class Voltage : BasePage
+    {
+        private static string ProjectVoltageUrl
+        {
+            get => $"https://www.serebii.net/music/projectvoltage/";
+        }
+    }
+}
