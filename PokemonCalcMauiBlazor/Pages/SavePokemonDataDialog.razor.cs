191644fb96c0e7fbada859d199d3df5a0ef05447
Iyarashii <strider1337v@gmail.com>
2025-03-20 18:31:48 +0100

    Cleanup usings

--- PokemonCalcMauiBlazor/Pages/SavePokemonDataDialog.razor.cs
+++ PokemonCalcMauiBlazor/Pages/SavePokemonDataDialog.razor.cs
@@ -3,11 +3,6 @@
 
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PkmnCalcMauiBlazor.Pages
 {
